b316946a447511ad2a1a5b83b4cb17f147672e7f
Hari Acharya <136120359+hariacharya80@users.noreply.github.com>
2024-06-08 11:15:27 +0545

    sample commit (#1)

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -10,4 +10,5 @@ mainRouter.get('/', indexPageGreeting);
 mainRouter.get('/books', getBooks);
 mainRouter.post('/book', addBook);
 mainRouter.post('/redis-cache', setRedisCache);
+
 export default mainRouter;
